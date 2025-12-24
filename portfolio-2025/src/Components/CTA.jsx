2413f46ae2e7b3be476e66aa45a770107c8c1d64
hajarnouby <hajarnoubyart@gmail.com>
2025-12-24 15:39:31 +0200

    fix: CTA animation

--- portfolio-2025/src/Components/CTA.jsx
+++ portfolio-2025/src/Components/CTA.jsx
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
-import './CTA.css'
-import arrow from '../Assets/icons/arrow.svg'
+// import React, { Component } from 'react';
+// import './CTA.css'
+// import arrow from '../Assets/icons/arrow.svg'
+// const Cta = (props) => {
+//     return (  
+//             <div className='fnt1 st1 cta row'>
+//                 <h4 className='txt2 txt4'>{props.title}</h4>
+//                 <p className='txt2 txt4'>{props.value}</p>
+//                 <img className='ico' src={arrow} alt="arrow" />
+//             </div>
+
+//     );
+// }
+ 
+// export default Cta;
+import React from 'react';
+import './CTA.css';
+import arrow from '../Assets/icons/arrow.svg';
+
 const Cta = (props) => {
     return (  
-            <div className='fnt1 st1 cta row'>
-                <h4 className='txt2 txt4'>{props.title}</h4>
-                <p className='txt2 txt4'>{props.value}</p>
-                <img className='ico' src={arrow} alt="arrow" />
-            </div>
-
+        <div className='fnt1 st1 cta row'>
+            <h4 className='txt2 txt4'>{props.title}</h4>
+            <p className='txt2 txt4'>{props.value}</p>
+            <img className='ico' src={arrow} alt="arrow" />
+        </div>
     );
 }
- 
+
 export default Cta;
